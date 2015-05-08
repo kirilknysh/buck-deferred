7d3bacfb58787fac4006fafca0f0142ce3dc347f
unknown <Kiril_Knysh@epam.com>
2015-05-08 14:06:03 +0300

    Add basic API;

---
+++ src/buck-deferred.js
@@ -0,0 +1,65 @@
+;(function (global, $) {
+    if (!$) {
+        $ = {};
+    }
+
+    $.Deferred = function (func) {
+
+    };
+
+    $.Deferred.prototype.state = function () {
+
+    };
+
+    $.Deferred.prototype.then = function () {
+
+    };
+
+    $.Deferred.prototype.fail = function () {
+
+    };
+
+    $.Deferred.prototype.always = function () {
+
+    };
+
+    $.Deferred.prototype.done = function () {
+
+    };
+
+    $.Deferred.prototype.resolve = function () {
+
+    };
+
+    $.Deferred.prototype.resolveWith = function () {
+
+    };
+
+    $.Deferred.prototype.reject = function () {
+
+    };
+
+    $.Deferred.prototype.rejectWith = function () {
+
+    };
+
+    $.Deferred.prototype.notify = function () {
+
+    };
+
+    $.Deferred.prototype.notifyWith = function () {
+
+    };
+
+    $.Deferred.prototype.progress = function () {
+
+    };
+
+    $.Deferred.prototype.promise = function () {
+
+    };
+
+    $.when = function (/* dfds */) {
+
+    };
+})(window, $);
