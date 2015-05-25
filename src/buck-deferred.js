253fc3d87558f7ed4d444a8872296d5d38131634
unknown <Kiril_Knysh@epam.com>
2015-05-25 19:44:55 +0300

    Exclude Promise; Add basic implementations;

--- src/buck-deferred.js
+++ src/buck-deferred.js
@@ -1,65 +1,140 @@
 ;(function (global, $) {
-    if (!$) {
-        $ = {};
-    }
-
-    $.Deferred = function (func) {
-
+    var STATE = {
+        PENDING: 'pending',
+        RESOLVED: 'resolved',
+        REJECTED: 'rejected'
     };
 
-    $.Deferred.prototype.state = function () {
+    function Promise() {
+        this.successCallbacks = [];
+        this.failCallbacks = [];
+        this.progressCallbacks = [];
 
+        this._state = STATE.PENDING;
+    }
+
+    Promise.prototype.state = function () {
+        return this._state;
+    };
+
+    Promise.prototype.then = function (fnDone, fnFail, fnProgress) {
+        this.done(fnDone);
+        this.fail(fnDone);
+        this.progress(fnProgress);
+    };
+
+    Promise.prototype.fail = function (/* fns */) {
+        var iterator = 0;
+
+        for (; iterator < arguments.length; iterator++) {
+            if (typeof arguments[iterator] === 'function') {
+                this.failCallbacks.push(arguments[iterator]);
+            }
+        }
+    };
+
+    Promise.prototype.always = function (/* fns */) {
+        var iterator = 0;
+
+        for (; iterator < arguments.length; iterator++) {
+            if (typeof arguments[iterator] === 'function') {
+                this.successCallbacks.push(arguments[iterator]);
+                this.failCallbacks.push(arguments[iterator]);
+            }
+        }
+    };
+
+    Promise.prototype.done = function (/* fns */) {
+        var iterator = 0;
+
+        for (; iterator < arguments.length; iterator++) {
+            if (typeof arguments[iterator] === 'function') {
+                this.successCallbacks.push(arguments[iterator]);
+            }
+        }
+    };
+
+    Promise.prototype.progress = function (/* fns */) {
+        var iterator = 0;
+
+        for (; iterator < arguments.length; iterator++) {
+            if (typeof arguments[iterator] === 'function') {
+                this.progressCallbacks.push(arguments[iterator]);
+            }
+        }
+    };
+
+
+    $.Deferred = function (func) {
+        this.promise = new Promise();
+
+        if (typeof func === 'function') {
+            func.call(this, this);
+        }
     };
 
     $.Deferred.prototype.then = function () {
-
+        // chaining ???
     };
 
-    $.Deferred.prototype.fail = function () {
+    $.Deferred.prototype.fail = function (/* fns */) {
+        this.promise.fail.apply(this.promise, arguments);
 
+        return this;
     };
 
-    $.Deferred.prototype.always = function () {
+    $.Deferred.prototype.always = function (/* fns */) {
+        this.promise.always.apply(this.promise, arguments);
 
+        return this;
     };
 
-    $.Deferred.prototype.done = function () {
+    $.Deferred.prototype.done = function (/* fns */) {
+        this.promise.done.apply(this.promise, arguments);
 
+        return this;
+    };
+
+    $.Deferred.prototype.progress = function (/* fns */) {
+        this.promise.progress.apply(this.promise, arguments);
+
+        return this;
     };
 
     $.Deferred.prototype.resolve = function () {
-
+        return this.resolveWith(this.promise, arguments);
     };
 
-    $.Deferred.prototype.resolveWith = function () {
+    $.Deferred.prototype.resolveWith = function (context, args) {
+        this.promise._state = STATE.RESOLVED;
 
+        return this;
     };
 
     $.Deferred.prototype.reject = function () {
-
+        return this.rejectWith(this.promise, arguments);
     };
 
-    $.Deferred.prototype.rejectWith = function () {
+    $.Deferred.prototype.rejectWith = function (context, args) {
+        this.promise._state = STATE.REJECTED;
 
+        return this;
     };
 
     $.Deferred.prototype.notify = function () {
-
+        return this.notifyWith(this.promise, arguments);
     };
 
-    $.Deferred.prototype.notifyWith = function () {
-
+    $.Deferred.prototype.notifyWith = function (context, args) {
+        return this;
     };
 
-    $.Deferred.prototype.progress = function () {
-
-    };
-
-    $.Deferred.prototype.promise = function () {
-
+    $.Deferred.prototype.promise = function (obj) {
+        //do we need promise non-objects??
+        return typeof obj === 'object' ? obj/* mixin promise into passed obj */ : this.promise;
     };
 
     $.when = function (/* dfds */) {
 
     };
-})(window, $);
+})(this, this.$ = this.$ || {});
