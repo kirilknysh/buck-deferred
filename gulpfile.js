69af40b1e88bb264d92a25fa9590574893bd5d30
Kiril Knysh <k.knysh@gmail.com>
2015-05-30 13:00:25 +0300

    Add comments to build file;

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    wrap = require("gulp-wrap");
 
 gulp.task('minify', function () {
     gulp.src('src/buck-deferred.js')
@@ -13,6 +14,7 @@ gulp.task('build', function () {
     gulp.src('src/buck-deferred.js')
         .pipe(gulp.dest('build'))
         .pipe(uglify())
+        .pipe(wrap("//Build version of buck-deferred\n<%= contents %>"))
         .pipe(rename('buck-deferred.min.js'))
         .pipe(gulp.dest('build'));
 });
